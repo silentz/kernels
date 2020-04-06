d398be9fbd3a74d59ace69b555f8b830ed8b229a
Max <mepershin@gmail.com>
2020-04-06 16:59:10 +0300

    bigfix2

--- 03-sys_call/contact_storage/storage.c
+++ 03-sys_call/contact_storage/storage.c
@@ -7,19 +7,19 @@
 
 // ========== [Local functions] ==========
 
-static int add_user_note(const char __user *, const char __user *, const char __user *, const char __user *);
+static int add_user_note(const struct user_data *);
 static int get_user_note(const char *);
 static int delete_user_note(const char *);
-static int copy_string(char **, const char __user *);
+static int copy_string(char *, const char __user *);
 
 
 // ========== [Structs] ===========
 
 static struct User {
-    char *name;
-    char *surname;
-    char *phone;
-    char *email;
+    char name[USER_DATA_INPUT_MAX];
+    char surname[USER_DATA_INPUT_MAX];
+    char phone[USER_DATA_INPUT_MAX];
+    char email[USER_DATA_INPUT_MAX];
     char status;
 };
 
@@ -37,9 +37,8 @@ static int initialized = 0;
 
 SYSCALL_DEFINE3(get_user, const char __user *, surname, unsigned int, len, struct user_data __user *, output_data) {
     char *local_surname;
-    User current_user;
     int result_index;
-    struct user_data result;
+    int index;
 
     if (initialized == 0) {
         for (index = 0; index < USER_COUNT; ++index) {
@@ -68,43 +67,14 @@ SYSCALL_DEFINE3(get_user, const char __user *, surname, unsigned int, len, struc
 
     if (result_index == -1) {
         printk(KERN_INFO "get_user: surname not found\n");
-        kfree(local_surname);
         return -1;
     }
 
-    current_user = users[result_index];
-    result.name = (char*) kmalloc(strlen(current_user.name) + 1, GFP_USER);
-    result.surname = (char*) kmalloc(strlen(current_user.surname) + 1, GFP_USER);
-    result.phone = (char*) kmalloc(strlen(current_user.phone) + 1, GFP_USER);
-    result.email = (char*) kmalloc(strlen(current_user.email) + 1, GFP_USER);
-
-    if (result.name == NULL || result.surname == NULL || result.phone == NULL || result.email == NULL) {
-        printk(KERN_ERR "get_user: not enough memory\n");
-        kfree(result.name);
-        kfree(result.surname);
-        kfree(result.phone);
-        kfree(result.email);
-        return -1;
-    }
-
-    if (copy_to_user(result.name, current_user.name, strlen(current_user.name) + 1) != 0 ||
-            copy_to_user(result.surname, current_user.surname, strlen(current_user.surname) + 1) != 0 ||
-            copy_to_user(result.email, current_user.email, strlen(current_user.email) + 1) != 0 ||
-            copy_to_user(result.phone, current_user.phone, strlen(current_user.phone) + 1) != 0) {
+    if (copy_to_user(output_data->name, users[result_index].name, USER_DATA_INPUT_MAX) != 0 ||
+            copy_to_user(output_data->surname, users[result_index].surname, USER_DATA_INPUT_MAX) != 0 ||
+            copy_to_user(output_data->email, users[result_index].email, USER_DATA_INPUT_MAX) != 0 ||
+            copy_to_user(output_data->phone, users[result_index].phone, USER_DATA_INPUT_MAX) != 0) {
         printk(KERN_ERR "get_user: can't copy data to userspace\n");
-        kfree(result.name);
-        kfree(result.surname);
-        kfree(result.phone);
-        kfree(result.email);
-        return -1;
-    }
-
-    if (copy_to_user(output_data, &result, sizeof(struct user_data)) != 0) {
-        printk(KERN_ERR "get_user: can't copy data to userspace\n");
-        kfree(result.name);
-        kfree(result.surname);
-        kfree(result.phone);
-        kfree(result.email);
         return -1;
     }
 
@@ -113,7 +83,7 @@ SYSCALL_DEFINE3(get_user, const char __user *, surname, unsigned int, len, struc
 
 
 SYSCALL_DEFINE1(add_user, struct user_data __user *, data) {
-    struct user_data current_user;
+    struct user_data *current_user;
     int index;
 
     if (initialized == 0) {
@@ -124,23 +94,28 @@ SYSCALL_DEFINE1(add_user, struct user_data __user *, data) {
     }
 
     printk(KERN_INFO "add_user: called\n");
+    current_user = (struct user_data*) kmalloc(sizeof(struct user_data), GFP_KERNEL);
 
-    if (copy_from_user(&current_user, data, sizeof(struct user_data)) != 0) {
+    if (copy_from_user(current_user, data, sizeof(struct user_data)) != 0) {
         printk(KERN_ERR "add_user: can't copy from user\n");
+        kfree(current_user);
         return -1;
     }
 
-    if (add_user_note(current_user.name, current_user.surname, current_user.phone, current_user.email) != 0) {
+    if (add_user_note(current_user) != 0) {
         printk(KERN_ERR "add_user: can't add new user\n");
+        kfree(current_user);
         return -1;
     }
 
+    kfree(current_user);
     return 0;
 }
 
 
 SYSCALL_DEFINE2(del_user, const char __user *, surname, unsigned int, len) {
     char *local_surname;
+    int index;
 
     if (initialized == 0) {
         for (index = 0; index < USER_COUNT; ++index) {
@@ -178,34 +153,33 @@ SYSCALL_DEFINE2(del_user, const char __user *, surname, unsigned int, len) {
 // ========== [Local function implementation] ==========
 
 
-static int copy_string(char **result, const char __user *data) {
+static int copy_string(char *result, const char *data) {
     size_t size;
+    int index;
 
-    size = strnlen_user(data, MAX_INPUT_SIZE);
-    *result = (char*) kmalloc(size + 1, GFP_KERNEL);
-    if (copy_from_user(*result, data, size) != 0) {
+    size = strlen(data);
+    if (size > MAX_INPUT_SIZE) {
         return -1;
     }
-    (*result)[size] = '\0';
+
+    for (index = 0; index < USER_DATA_INPUT_MAX; ++index) {
+        result[index] = data[index];
+    }
+    result[size] = '\0';
 
     return 0;
 }
 
 
-static int add_user_note(const char __user *name, const char __user *surname, const char __user *phone,
-        const char __user *email) {
+static int add_user_note(const struct user_data *current_user) {
     int index;
 
     for (index = 0; index < USER_COUNT; ++index) {
         if (users[index].status == 'f') { // check if user cell is free
-            if (copy_string(&users[index].name, name) != 0 ||
-                    copy_string(&users[index].surname, surname) != 0 ||
-                    copy_string(&users[index].phone, phone) != 0 ||
-                    copy_string(&users[index].email, email) != 0) {
-                kfree(users[index].name);
-                kfree(users[index].surname);
-                kfree(users[index].phone);
-                kfree(users[index].email);
+            if (copy_string(users[index].name, current_user->name) != 0 ||
+                    copy_string(users[index].surname, current_user->surname) != 0 ||
+                    copy_string(users[index].phone, current_user->phone) != 0 ||
+                    copy_string(users[index].email, current_user->email) != 0) {
                 return -1;
             }
             users[index].status = 't'; // set "taken" status
@@ -219,41 +193,27 @@ static int add_user_note(const char __user *name, const char __user *surname, co
 
 static int get_user_note(const char *surname) {
     int index;
-    int result;
 
-    result = -1; // NOT FOUND
     for (index = 0; index < USER_COUNT; ++index) {
         if ((users[index].status == 't') && (strcmp(surname, users[index].surname) == 0)) {
-            result = index;
-            break;
+            return index;
         }
     }
 
-    return result;
+    return -1; // NOT FOUND
 }
 
 
 static int delete_user_note(const char *surname) {
     int index;
-    int result;
 
-    result = -1;
     for (index = 0; index < USER_COUNT; ++index) {
         if ((users[index].status == 't') && (strcmp(surname, users[index].surname) == 0)) {
-            result = index;
-            break;
+            users[index].status = 'f';
+            return index;
         }
     }
 
-    if (result == -1) { // NOT FOUND
-        return -1;
-    }
-
-    kfree(users[result].name);
-    kfree(users[result].surname);
-    kfree(users[result].phone);
-    kfree(users[result].email);
-    users[result].status = 'f';
-    return 0;
+    return -1;
 }
 
--- 03-sys_call/include/linux/user_data.h
+++ 03-sys_call/include/linux/user_data.h
@@ -1,11 +1,13 @@
 #ifndef INCLUDE_LINUX_USER_DATA
 #define INCLUDE_LINUX_USER_DATA
 
+#define USER_DATA_INPUT_MAX 1024
+
 struct user_data {
-    char *name;
-    char *surname;
-    char *phone;
-    char *email;
+    char name[USER_DATA_INPUT_MAX];
+    char surname[USER_DATA_INPUT_MAX];
+    char phone[USER_DATA_INPUT_MAX];
+    char email[USER_DATA_INPUT_MAX];
 };
 
 #endif
